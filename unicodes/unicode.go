2529f5419a5bd9adb5af0809dd472b73fef0c274
Kianoosh <kianoosh.ashayerizade@snapp.cab>
2023-01-07 13:28:09 +0330

    add unicodes package

---
+++ unicodes/unicode.go
@@ -0,0 +1,10 @@
+package unicodes
+
+const (
+	Backslash       = rune(92)
+	SingleQuotation = rune(39)
+	Space           = rune(32)
+	Tab             = rune(9)
+	NewLine         = rune(10)
+	Dot             = rune(46)
+)
