fa6cb069d70d744650d4b2da05c9904a75e7019c
Ali Naseri <mo.ali.naseri@gmail.com>
2023-01-13 19:17:39 +0330

    add comments

--- character/ASCII.go
+++ character/ASCII.go
@@ -1,16 +1,16 @@
 package character
 
 const (
-	RParent         = rune(41)
-	Star            = rune(42)
-	Slash           = rune(47)
-	Backslash       = rune(92)
-	SingleQuotation = rune(39)
-	Space           = rune(32)
-	Tab             = rune(9)
-	NewLine         = rune(10)
-	Dot             = rune(46)
-	CarriageReturn  = rune(13)
-	LCurlyBrace     = rune(123)
-	RCurlyBrace     = rune(125)
+	RParent         = rune(41)  // )
+	Star            = rune(42)  // *
+	Slash           = rune(47)  // /
+	Backslash       = rune(92)  // \
+	SingleQuotation = rune(39)  // '
+	Space           = rune(32)  // space
+	Tab             = rune(9)   // tab
+	NewLine         = rune(10)  // new line
+	Dot             = rune(46)  // .
+	CarriageReturn  = rune(13)  // carriage return
+	LCurlyBrace     = rune(123) // {
+	RCurlyBrace     = rune(125) // }
 )
